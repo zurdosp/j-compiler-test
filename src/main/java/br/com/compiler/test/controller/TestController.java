1922a25ffd07f49e73e9e900d28355ded7d3a731
Christian Oscar Tejada Pacheco <zurdosp@gmail.com>
2019-07-16 11:54:16 -0300

    Update TestController.java

--- src/main/java/br/com/compiler/test/controller/TestController.java
+++ src/main/java/br/com/compiler/test/controller/TestController.java
@@ -1,7 +1,17 @@
 package br.com.compiler.test.controller;
 
+import java.io.Console;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.mdkt.compiler.InMemoryJavaCompiler;
 import org.slf4j.Logger;
@@ -13,6 +23,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.compiler.test.model.ParamTestCode;
 import br.com.compiler.test.model.ResponseTestCode;
 import io.swagger.annotations.Api;
 
@@ -24,38 +35,55 @@ public class TestController {
 
 	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
-	@PostMapping("/test")
+	@PostMapping("/execute")
 	@ResponseBody
-	public ResponseTestCode applyTest(@RequestBody String sourceCode, @RequestBody String sourceTest) throws Exception {
+	public ResponseTestCode executeTest(@RequestBody ParamTestCode paramTestCode) throws Exception {
 
+		File outputFileName = new File("outExecute.txt");
+		FileOutputStream fos = new FileOutputStream(outputFileName);
+		PrintStream ps = new PrintStream(fos);
+		System.setOut(ps);
 		try {
-			StringBuffer sourceCodeBuffer = new StringBuffer();
-			sourceCodeBuffer.append("package br.com.compiler.test.controller;\n");
-			sourceCodeBuffer.append("public class MainTest1 {\n");
-			sourceCodeBuffer.append("   public static void main(String args[]) { System.out.println(\"will\"); }");
-			sourceCodeBuffer.append("}");
-
-			Class<?> testClass = InMemoryJavaCompiler.newInstance().compile("br.com.compiler.test.controller.MainTest1", sourceCode);
-
+			Class<?> testClass = InMemoryJavaCompiler.newInstance().compile("br.com.compiler.test.controller.MainTest", paramTestCode.getSourceMainToTest());
 			Method sumInstanceMethod = testClass.getMethod("main", String[].class);
-			Object[] obj = new Object[1];
-			sumInstanceMethod.invoke(testClass, obj);
+			String[] mainParams = new String[2];
+			mainParams[0] = paramTestCode.getParams().get(0);
+			mainParams[1] = paramTestCode.getParams().get(1);
+			sumInstanceMethod.invoke(testClass, (Object) mainParams);
 		} catch (Exception e) {
+			Console con = System.console();
+			System.out.println(con);
 			logger.error("Erro ao tentar compilar codigo ", e);
-			//return e.getMessage().toString().split(",")[2].toString();
-			return new ResponseTestCode();
+			ResponseTestCode responseTestCode = new ResponseTestCode();
+			responseTestCode.setMessage(trataException(e));
+			return responseTestCode;
 		}
-
-		PrintStream console = System.out;
-		//return console.toString();
-		return new ResponseTestCode();
+		ResponseTestCode responseTestCode = new ResponseTestCode();
+		responseTestCode.setCode(1);
+		String finalMsg = processFinalMessage(outputFileName).toString();
+		responseTestCode.setMessage(finalMsg);
+		return responseTestCode;
 	}
 
-	@PostMapping("/test2")
-	@ResponseBody
-	public ResponseTestCode applyTest2(@RequestBody String sourceCode, @RequestBody String sourceTest) throws Exception {
-		ResponseTestCode responseTestCode = new ResponseTestCode();
-		return responseTestCode;
+	private List processFinalMessage(File outputFileName) {
+		List<String> list = new ArrayList<>();
+
+		try (Stream<String> stream = Files.lines(Paths.get(outputFileName.getName()))) {
+			list = stream.collect(Collectors.toList());
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return list;
+	}
+
+	private String trataException(Exception e) {
+
+		if (e.getMessage().toString().split(",").length > 1) {
+			return e.getMessage().toString().split(",")[2].toString().replace("message=", "").substring(0, e.getMessage().toString().split(",")[2].toString().replace("message=", "").length() - 1);
+		}
+		return e.getMessage().toString().split(",")[0].toString().replace("message=", "").substring(0, e.getMessage().toString().split(",")[0].toString().replace("message=", "").length() - 1);
 	}
 
 }
