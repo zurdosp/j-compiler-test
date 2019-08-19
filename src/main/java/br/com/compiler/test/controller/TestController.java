8f39814b7e9ab032596f81f3f16ff7ada9182053
Christian Oscar Tejada Pacheco <zurdosp@gmail.com>
2019-08-19 18:23:44 -0300

    Update TestController.java

--- src/main/java/br/com/compiler/test/controller/TestController.java
+++ src/main/java/br/com/compiler/test/controller/TestController.java
@@ -13,6 +13,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import org.apache.commons.lang.StringUtils;
 import org.mdkt.compiler.InMemoryJavaCompiler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -37,28 +38,44 @@ public class TestController {
 
 	private static final Logger logger = LoggerFactory.getLogger(TestController.class);
 
+	private static final String argsMainParams = "args[";
+
 	@ApiImplicitParams({ @ApiImplicitParam(name = "sourceMainToTest", value = "package br.com.compiler.test.controller;public class MainTest {public static void main(String[] args) {System.out.println(Integer.valueOf(args[0]) + Integer.valueOf(args[1])); System.out.println(args[0]);}}"), @ApiImplicitParam(name = "params", value = "\"1\", \"2\"")
 	})
 	@PostMapping("/execute")
 	@ResponseBody
 	public ResponseTestCode executeTest(@RequestBody ParamTestCode paramTestCode) throws Exception {
-
+		logger.error("Trying to compile code: " + paramTestCode);
+		String className = paramTestCode.getSourceMainToTest().substring(paramTestCode.getSourceMainToTest().indexOf("public class") + 13, paramTestCode.getSourceMainToTest().indexOf("{")).trim();
 		File outputFileName = new File("outExecute.txt");
 		FileOutputStream fos = new FileOutputStream(outputFileName);
 		PrintStream ps = new PrintStream(fos);
 		System.setOut(ps);
 		try {
-			Class<?> testClass = InMemoryJavaCompiler.newInstance().compile("br.com.compiler.test.controller.MainTest", paramTestCode.getSourceMainToTest());
+			Class<?> testClass = InMemoryJavaCompiler.newInstance().compile("br.com.compiler.test.controller.".concat(className), paramTestCode.getSourceMainToTest());
 			Method sumInstanceMethod = testClass.getMethod("main", String[].class);
-			String[] mainParams = new String[2];
-			mainParams[0] = paramTestCode.getParams().get(0).split(",")[0];
-			mainParams[1] = paramTestCode.getParams().get(0).split(",")[1];
+			int argOccurrencesCount = StringUtils.countMatches(paramTestCode.getSourceMainToTest(), argsMainParams);
+			String[] testeCodeParams = paramTestCode.getParams().get(0).split(",");
+
+			if (testeCodeParams.length != argOccurrencesCount) {
+				ResponseTestCode responseTestCode = new ResponseTestCode();
+				responseTestCode.setCode(0);
+				responseTestCode.setMessage("number of params tests mismatch");
+				return responseTestCode;
+			}
+
+			String[] mainParams = new String[argOccurrencesCount];
+			for (int i = 0; i < argOccurrencesCount; i++) {
+				mainParams[i] = testeCodeParams[i].trim();
+			}
+
 			sumInstanceMethod.invoke(testClass, (Object) mainParams);
 		} catch (Exception e) {
 			Console con = System.console();
 			System.out.println(con);
 			logger.error("Erro ao tentar compilar codigo ", e);
 			ResponseTestCode responseTestCode = new ResponseTestCode();
+			responseTestCode.setCode(0);
 			responseTestCode.setMessage(trataException(e));
 			return responseTestCode;
 		}
